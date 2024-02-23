c20ff47a4de678df65162c6b624e6dd39cc31040
volodymyr <atanov.v@gmail.com>
2024-02-23 16:57:41 +0100

    Small changes

--- src/test/java/loginTest/LoginTest.java
+++ src/test/java/loginTest/LoginTest.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
     // Create webDriver instance

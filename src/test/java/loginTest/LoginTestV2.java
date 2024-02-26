d7f4230dbe5bcb14d7a761739f15131e3c1d7276
volodymyr <atanov.v@gmail.com>
2024-02-26 23:30:43 +0100

    LoginTestV2 extended with all verifications of Header.

--- src/test/java/loginTest/LoginTestV2.java
+++ src/test/java/loginTest/LoginTestV2.java
@@ -1,6 +1,5 @@
 package loginTest;
 
-import data.TestData;
 import org.junit.Test;
 
 import static data.TestData.LOGIN_DEFAULT;
@@ -10,11 +9,18 @@ public class LoginTestV2 extends baseTest.BaseTest{
     @Test
     public void validLoginTest() {
         pageProvider.getLoginPage().openLoginPage();
-        pageProvider.getLoginPage().clickLoginButtonInHeader();
+        pageProvider.getLoginPage().getHeader().clickLoginButton();
         pageProvider.getLoginPage().enterEmail(LOGIN_DEFAULT);
         pageProvider.getLoginPage().enterPassword(PASSWORD_DEFAULT);
         pageProvider.getLoginPage().clickLoginButtonInModal();
 
+        pageProvider.getHomePage().getHeader().checkIsLogoDisplayed();
+        pageProvider.getHomePage().getHeader().checkIsHomeButtonDisplayed();
+        pageProvider.getHomePage().getHeader().checkIsAboutButtonDisplayed();
+        pageProvider.getHomePage().getHeader().checkIsContactButtonDisplayed();
+        pageProvider.getHomePage().getHeader().checkIsHelpButtonDisplayed();
         pageProvider.getHomePage().getHeader().checkIsDropdownMenuDisplayed();
+
+        pageProvider.getHomePage().getHeader().checkIsLoginButtonNotDisplayed();
     }
 }

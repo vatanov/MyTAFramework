d16e24ef8dd9e00297a8c48a327d130e0e9910a3
volodymyr <atanov.v@gmail.com>
2024-02-26 23:25:35 +0100

    Header extended with all elements.

--- src/main/java/pages/elements/Header.java
+++ src/main/java/pages/elements/Header.java
@@ -6,13 +6,102 @@ import org.openqa.selenium.support.FindBy;
 import pages.ActionsWithElements;
 
 public class Header extends ActionsWithElements {
+    @FindBy(xpath = "//img[@class='logomain']")
+    private WebElement logo;
+
+    @FindBy(xpath = "//span[@class='glyphicon glyphicon-home']")
+    private WebElement homeButton;
+
+    @FindBy(xpath = "//a[@href='about.php']")
+    private WebElement aboutButton;
+
+    @FindBy(xpath = "//a[@href='contact.php']")
+    private WebElement contactButton;
+
+    @FindBy(xpath = "//a[@href='help.php']")
+    private WebElement helpButton;
+
+    @FindBy(xpath = "//a[@data-target='#login']")
+    private WebElement loginButton;
+
+    @FindBy(xpath = "//a[@data-target='#register']")
+    private WebElement registerButton;
+
     @FindBy(xpath = "//a[@data-toggle='dropdown']")
     private WebElement dropdownMenu;
+
+    @FindBy(xpath = "//a[@href='logout.php']")
+    private WebElement logoutButton;
+
     public Header(WebDriver webDriver) {
         super(webDriver);
     }
 
+    public void checkIsLogoDisplayed() {
+        checkElementDisplayed(logo);
+    }
+
+    public void checkIsHomeButtonDisplayed() {
+        checkElementDisplayed(homeButton);
+    }
+
+    public void checkIsAboutButtonDisplayed() {
+        checkElementDisplayed(aboutButton);
+    }
+
+    public void checkIsContactButtonDisplayed() {
+        checkElementDisplayed(contactButton);
+    }
+
+    public void checkIsHelpButtonDisplayed() {
+        checkElementDisplayed(helpButton);
+    }
+
+    public void checkIsLoginButtonDisplayed() {
+        checkElementDisplayed(loginButton);
+    }
+
+    public void checkIsLoginButtonNotDisplayed() {
+       checkElementNotDisplayed(loginButton);
+    }
+
+    public void checkIsRegisterButtonDisplayed() {
+        checkElementDisplayed(registerButton);
+    }
+
     public void checkIsDropdownMenuDisplayed() {
         checkElementDisplayed(dropdownMenu);
+    }
+
+    public void clickHomeButton() {
+        clickOnElement(homeButton);
+    }
+
+    public void clickAboutButton() {
+        clickOnElement(aboutButton);
+    }
+
+    public void clickContactButton() {
+        clickOnElement(contactButton);
+    }
+
+    public void clickHelpButton() {
+        clickOnElement(helpButton);
+    }
+
+    public void clickLoginButton() {
+        clickOnElement(loginButton);
+    }
+
+    public void clickRegisterButton() {
+        clickOnElement(registerButton);
+    }
+
+    public void clickDropdownMenu() {
+        clickOnElement(dropdownMenu);
+    }
+
+    public void clickLogout() {
+        clickOnElement(logoutButton);
     }
 }

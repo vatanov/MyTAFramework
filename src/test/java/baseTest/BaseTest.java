ac80a80a7d77ce401c8509c7af9e04146cba17e5
volodymyr <atanov.v@gmail.com>
2024-03-19 23:13:38 +0100

    Added browsers.

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -9,6 +9,9 @@ import org.junit.Rule;
 import org.junit.rules.TestName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import pages.PageProvider;
 
 import java.time.Duration;
@@ -25,8 +28,9 @@ public class BaseTest {
     public void setUp() {
         logger.info("\n");
         logger.info("=====> Setup for Test " + testName.getMethodName() + " is started");
-        WebDriverManager.chromedriver().clearDriverCache().setup();
-        webDriver = new ChromeDriver();
+        //WebDriverManager.chromedriver().clearDriverCache().setup();
+        //webDriver = new ChromeDriver();
+        webDriver = initDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(ConfigProvider.configProperties.TIME_FOR_DEFAULT_WAIT()));
         logger.info("Browser is opened");
@@ -42,5 +46,26 @@ public class BaseTest {
         webDriver.quit();
         logger.info("Browser is closed");
         logger.info("=====> Tear down for Test " + testName.getMethodName() + " is finished");
+    }
+
+    // This method is used to initialize the WebDriver with the browser specified in the system property
+    private WebDriver initDriver() {
+        String browser = System.getProperty("browser");
+        if ((browser == null) || browser.equalsIgnoreCase("chrome")) { // default browser -Dbrowser=chrome
+            WebDriverManager.chromedriver().clearDriverCache().setup();
+            webDriver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) { // -Dbrowser=firefox
+            WebDriverManager.firefoxdriver().clearDriverCache().setup();
+            webDriver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("edge")) { // -Dbrowser=edge
+            WebDriverManager.edgedriver().clearDriverCache().setup();
+            webDriver = new EdgeDriver();
+        } else if (browser.equalsIgnoreCase("safari")) {
+            WebDriverManager.safaridriver().clearDriverCache().setup();
+            webDriver = new SafariDriver();
+        } else {
+            throw new IllegalArgumentException("Browser " + browser + " is not supported");
+        }
+        return webDriver;
     }
 }

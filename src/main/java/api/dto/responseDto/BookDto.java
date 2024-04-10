e4e1d21fa407b2417579f6aa36967a844ac2e5ac
volodymyr <atanov.v@gmail.com>
2024-04-10 22:30:03 +0200

    Added DTOs. Test extended with several assertions.

---
+++ src/main/java/api/dto/responseDto/BookDto.java
@@ -0,0 +1,114 @@
+package api.dto.responseDto;
+
+public class BookDto {
+    String isbn;
+    String title;
+    String subTitle;
+    String author;
+    String publish_date;
+    String publisher;
+    int pages;
+    String description;
+    String website;
+
+    public BookDto(String isbn, String title, String subTitle, String author, String publisher, int pages, String description) {
+        this.isbn = isbn;
+        this.title = title;
+        this.subTitle = subTitle;
+        this.author = author;
+        this.publisher = publisher;
+        this.pages = pages;
+        this.description = description;
+    }
+
+    public BookDto(){
+
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublish_date() {
+        return publish_date;
+    }
+
+    public void setPublish_date(String publish_date) {
+        this.publish_date = publish_date;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    @Override
+    public String toString() {
+        return "BookDto{" +
+                "isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", subTitle='" + subTitle + '\'' +
+                ", author='" + author + '\'' +
+                ", publish_date='" + publish_date + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", pages=" + pages +
+                ", description='" + description + '\'' +
+                ", website='" + website + '\'' +
+                '}';
+    }
+}
--- src/test/java/apiTests/ApiTest.java
+++ src/test/java/apiTests/ApiTest.java
@@ -1,19 +1,25 @@
 package apiTests;
 
 import api.EndPoints;
-import io.restassured.RestAssured;
+import api.dto.responseDto.BookDto;
+import api.dto.responseDto.UsersBooksDto;
 import io.restassured.http.ContentType;
 import libs.ConfigProvider;
+import org.apache.log4j.Logger;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.oauth;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.everyItem;
 
 public class ApiTest {
     final String USER_ID = "e7faf904-e52e-43bd-b4d1-17c2eeb39a6d";
+    Logger logger = Logger.getLogger(getClass());
     @Test
     public void getBooksTest() {
-        given()
+        UsersBooksDto responseAsDto = given()
                 .headers(
                         "accept", ContentType.JSON,
                         "Authorization", "Bearer " + ConfigProvider.configHiddenProperties.api_token()
@@ -25,6 +31,48 @@ public class ApiTest {
                 .then()
                 .statusCode(200)
                 .log().all()
-        ;
+                .assertThat()
+                .body("username", equalTo("vova"))
+                .body("books[0].title", equalTo("Git Pocket Guide"))
+                // Verify field for each item in the list
+                .body("books.author", everyItem(equalTo("Richard E. Silverman")))
+                .extract().body().as(UsersBooksDto.class);
+
+        // Get whole json or some fields
+        logger.info(responseAsDto);
+        logger.info(responseAsDto.getUsername());
+        logger.info(responseAsDto.getBooks()[0].getTitle());
+
+        // Add asserts
+        // Simple assert
+        Assert.assertEquals("User ID is not expected.", USER_ID, responseAsDto.getUserId());
+
+        // Complex asserts
+        BookDto[] expectedBooksDto = {
+                new BookDto(
+                        "9781449325862",
+                        "Git Pocket Guide",
+                        "A Working Introduction",
+                        "Richard E. Silverman",
+                        "O'Reilly Media",
+                        234,
+                        "This pocket guide is the perfect on-the-job companion to Git, the distributed version control system. It provides a compact, readable introduction to Git for new users, as well as a reference to common commands and procedures for those of you with Git exp")
+        };
+        UsersBooksDto expectedUserBooksDto = new UsersBooksDto(USER_ID, "vova", expectedBooksDto);
+
+        Assert.assertEquals("Number of books ", expectedUserBooksDto.getBooks().length, responseAsDto.getBooks().length);
+
+        // Old manner with isEqualToIgnoringGivenFields method
+        SoftAssertions softAssertions = new SoftAssertions();
+        softAssertions.assertThat(responseAsDto.getUserId()).isEqualTo(expectedUserBooksDto.getUserId());
+        softAssertions.assertThat(responseAsDto.getUsername()).isEqualTo(expectedUserBooksDto.getUsername());
+
+        for (int i = 0; i < expectedUserBooksDto.getBooks().length; i++) {
+            softAssertions.assertThat(responseAsDto.getBooks()[i])
+                    .isEqualToIgnoringGivenFields(expectedUserBooksDto.getBooks()[i],
+                            "publish_date", "website");
+        }
+
+        softAssertions.assertAll();
     }
 }

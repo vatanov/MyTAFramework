fc0012e4642741a7af8c646805165548e99358b1
volodymyr <atanov.v@gmail.com>
2024-04-21 19:59:04 +0200

    Added Allure reports in API tests.

--- src/test/java/suits/SmokeSuite.java
+++ src/test/java/suits/SmokeSuite.java
@@ -1,5 +1,6 @@
 package suits;
 
+import apiTests.AddBookByApiTest;
 import loginTest.NewLoginTest;
 import logoutTest.LogoutTest;
 import org.junit.runner.RunWith;
@@ -8,7 +9,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         NewLoginTest.class,
-        LogoutTest.class
+        LogoutTest.class,
+        AddBookByApiTest.class
 })
 public class SmokeSuite {
 }
